7a02880ff53af7778221cdb23ac1abc312a5ecf6
Francisco PÉREZ <francisco.perez@solera.com>
2022-11-30 14:24:48 +0100

    "first layout completed"

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Listing;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//all listings
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading'=>'Latest Listings',
+        'listings'=> Listing::all()
+    ]);
+});
+
+//single listing
+
+Route::get('/listings/{id}', function ($id) {
+    return view('listing', [
+        
+        'listing'=> Listing::find($id)
+    ]);
 });
